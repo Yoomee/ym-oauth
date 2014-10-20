dd12bf173936fd10ddf267c5ff5488ed16d6f1fd
Greg Woodcock <greg@yoomee.com>
2014-10-20 16:18:56 +0100

    Wrap twitter registration callback in devise scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   #match ""
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
-  resources :registrations, :only => [] do
-    collection do
-      post 'twitter', :as => 'twitter'
+
+  devise_scope :user do
+    resources :registrations, :only => [] do
+      collection do
+        post 'twitter', :as => 'twitter'
+      end
     end
   end
-  
+
 end
